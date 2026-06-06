0a6ac65a716fcf2fb07a823cd300f04b072c8c00
kuzminkdk <kuzminkdk@gmail.com>
2026-06-06 19:44:24 +0300

    Update commands.sh

--- git/commands.sh
+++ git/commands.sh
@@ -1,19 +1,22 @@
 
 # — Clean —
+# After branch switching
 git clean -fdx
+# Also removes old submodules
+git clean -ffdx
 
 # — Rename branch —
 # Currently on the branch
 git branch -m <new-name>
-#On a different branch
+# On a different branch
 git branch -m <old-name> <new-name>
-#Push new name to remote
+# Push new name to remote
 git push origin -u <new-name>
-#Remove old name from remote
+# Remove old name from remote
 git push origin --delete <old-name>
 
 # — Rewrite history —
-git rebase -i HEAD~2
+git rebase -i HEAD~10
 
 # — Push new branch to remote —
 git push -u origin naked
